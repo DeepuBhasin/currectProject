d697cb9b8279641da9ba4e2e42d0acc9b3d37655
Deepinder Singh <Deepinder999@gmail.com>
2021-09-24 16:39:29 +0530

    working on crud app

--- ShipJsonAuth2/__db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = mysqli_connect('localhost','root','','database_auth');
-if(!$con){
-	die("Database Not Connected");
-}
-?>
--- uploadExcelFile/__db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = mysqli_connect('localhost','root','','rfid_api_database');
-if(!$con){
-	die("Database Not Connected");
-}
-?>
